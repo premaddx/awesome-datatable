a9baf78e803c631788e8bacc6dec8d020aaf2138
PREM_ANAND_TOPNO <prem.topno@instarem.com>
2019-12-09 12:28:40 +0530

    Updates

--- precache-manifest.0e8e764eb4a473bd6c61d370f7def5c3.js
+++ precache-manifest.0e8e764eb4a473bd6c61d370f7def5c3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08c37047a43494f57035fcd245af80fe",
+    "revision": "a9e6bda0e61111b2676c1e75b20a4fce",
     "url": "/awesome-datatable/index.html"
   },
   {
-    "revision": "ab6556529af7b41d54af",
+    "revision": "0e864e0515ffc39574dd",
     "url": "/awesome-datatable/static/css/main.beb752c2.chunk.css"
   },
   {
-    "revision": "6931ecca66d043eacb1f",
-    "url": "/awesome-datatable/static/js/2.a10460d6.chunk.js"
+    "revision": "676203b7ca83e6a1e4b7",
+    "url": "/awesome-datatable/static/js/2.adaab603.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/awesome-datatable/static/js/2.a10460d6.chunk.js.LICENSE"
+    "url": "/awesome-datatable/static/js/2.adaab603.chunk.js.LICENSE"
   },
   {
-    "revision": "ab6556529af7b41d54af",
-    "url": "/awesome-datatable/static/js/main.d2a0526d.chunk.js"
+    "revision": "0e864e0515ffc39574dd",
+    "url": "/awesome-datatable/static/js/main.a8ed3382.chunk.js"
   },
   {
     "revision": "0dc12aa85a7d2c51859a",
